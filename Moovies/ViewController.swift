8354fb94e7b531902d4dc8f6ffee52323e1c8b79
Bia Plutarco <Bia Plutarco>
2019-11-06 21:40:59 -0300

    remove coding test from ViewController

--- Moovies/ViewController.swift
+++ Moovies/ViewController.swift
@@ -12,37 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/upcoming?api_key=aa1f9a8cb654fe5383704dd771b128f0&language=pt-BR")!
-        
-
-        let session = URLSession.shared
-        let task = session.dataTask(with: url) { data, response, error in
-
-            if error != nil || data == nil {
-                print("Client error!")
-                return
-            }
-
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                print("Server error!")
-                return
-            }
-
-            guard let mime = response.mimeType, mime == "application/json" else {
-                print("Wrong MIME type!")
-                return
-            }
-
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print(json)
-            } catch {
-                print("JSON error: \(error.localizedDescription)")
-            }
-        }
-
-        task.resume()
     }
     
 }
