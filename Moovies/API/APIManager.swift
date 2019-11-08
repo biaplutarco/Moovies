39ba4cfecd02374f900afd41fd21c637d75bfe4a
Bia Plutarco <Bia Plutarco>
2019-11-08 13:14:43 -0300

    remove get upcoming movies

--- Moovies/API/APIManager.swift
+++ Moovies/API/APIManager.swift
@@ -14,55 +14,16 @@ class APIManager: NSObject {
     private let totalPages = 1
     private let baseURL = "https://api.themoviedb.org/3"
     private let key = "api_key=aa1f9a8cb654fe5383704dd771b128f0"
-    private let getDicoverMovies = "discover/movie?"
+    private let getDicoverMovies = "/discover/movie?"
     private let getGenresEndPoint = "/genre/movie/list?"
-    private let getUpComingMoviesEndPoint = "/movie/upcoming?"
     private let sortByPopularity = "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
     private let getFromGenres = "&with_genres="
     private let getInPTBR = "&language=pt-BR"
     
-    func getUpComingMovies(completion: @escaping (Result?) -> Void) {
-        for i in 1...totalPages {
-            let rawURL = "\(baseURL)\(getUpComingMoviesEndPoint)\(key)\(getInPTBR)&page=\(i)"
-            guard let url = URL(string: rawURL) else { fatalError("can't get the request") }
-            
-            var getRequest = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30)
-            getRequest.httpMethod = "GET"
-            
-            let getTask = URLSession.shared.dataTask(with: getRequest) { (data, response, error) in
-                
-                if error != nil {
-                    print("GET Request in \(getRequest)")
-                    print("Error: \(error!)")
-                }
-                
-                if data != nil {
-                    do {
-                        let resultObject = try JSONDecoder().decode(Result.self, from: data!)
-                        completion(resultObject)
-                        
-                    } catch let parserError {
-                        DispatchQueue.main.async {
-                            print(parserError)
-                            print("Unable to parse JSON response in \(getRequest)")
-                        }
-                    }
-                } else {
-                    print ("Received empty quest response from \(getRequest)")
-                }
-            }
-            
-            DispatchQueue.global(qos: .background).async {
-                getTask.resume()
-            }
-        }
-    }
-    
     func getURLMoviesFromGenres(_ genres: String) -> URL {
         let fromGenres = "\(getFromGenres)\(genres)"
         let rawURL = "\(baseURL)\(getDicoverMovies)\(key)\(getInPTBR)\(sortByPopularity)\(fromGenres)"
-        guard let urlEncoded = rawURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-            let url = URL(string: urlEncoded) else { fatalError("can't get the request") }
+        guard let url = URL(string: rawURL) else { fatalError("can't get the request") }
         
         return url
     }
